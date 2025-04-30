6bc2c3d56cfa9175aad77ba3bd317384e22daa09
Loic <loic.rey.vs@gmail.com>
2025-04-30 16:11:35 +0200

    ex01begining

---
+++ ex01/includes/ClapTrap.hpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lorey <lorey@student.42lausanne.ch>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/24 23:53:56 by lorey             #+#    #+#             */
+/*   Updated: 2025/04/26 23:34:05 by lorey            ###   LAUSANNE.ch       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+# include <iostream>
+# include <iomanip>
+# include <string>
+
+class ClapTrap
+{
+	private:
+		std::string	name;
+		int			hit_point;
+		int			max_hp;
+		int			energy_point;
+		int			attack_damage;
+		bool		is_dead;
+	public:
+		ClapTrap(std::string name);
+		ClapTrap(const ClapTrap &clap_trap);
+		~ClapTrap();
+		ClapTrap& operator=(const ClapTrap &clap_trap);
+		void	attack(const std::string &target);
+		void	takeDamage(unsigned int amount);
+		void	beRepaired(unsigned int amount);
+		void	displayInfo(void);
+};
+
+#endif
